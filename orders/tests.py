5928a18f570e7e7a4cfaaa3551b10ad0ec8ae010
bprabhati <prabhati.mili@gmail.com>
2025-03-07 13:29:13 +0530

    Fixed pytest issue by activating virtual env

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(username="testuser", password="password123")
+        self.assertEqual(user.username, "testuser")
